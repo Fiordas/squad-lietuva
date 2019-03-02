b3bbd1c33516c38feb926fb5dfda750af26b710b
Fiordas <simonas.juska@outlook.com>
2019-03-02 14:36:21 +0100

    Add mockup for main page content and Discord widget

---
+++ components/NewsBox.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class='box has-background-dark'>
+    <div class='content is-small has-text-light'>
+      <h1 class='has-text-light'>Pavadinimas</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices eleifend gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper dui. Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.</p>
+      <small class='is-pulled-right'>2019-03-02</small>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,47 @@
 <template>
   <div>
     <TheBanner/>
+    <section class='section'>
+      <div class='container'>
+        <div class='columns'>
+          <div class='column'>
+            <h1 class='title has-text-light'>Naujienos</h1>
+          </div>
+        </div>
+        <div class='columns'>
+          <div class='column'>
+            <NewsBox/>
+            <NewsBox/>
+            <NewsBox/>
+          </div>
+          <div class='column is-5-tablet is-4-desktop is-3-widescreen'>
+            <iframe src='https://discordapp.com/widget?id=246075715714416641&theme=dark' width='350' height='500' allowtransparency='true' frameborder='0'></iframe>
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
 import TheBanner from '@/components/TheBanner'
+import NewsBox from '@/components/NewsBox'
 
 export default {
   components: {
-    TheBanner
+    TheBanner,
+    NewsBox
   }
 }
 </script>
+
+
+<style scoped>
+section {
+  background-color: hsl(0, 0%, 8%);
+}
+iframe {
+  min-height: 500px;
+}
+</style>
+
