4d9a290f5c5a6d21cd44b1d6c70537b1c07eb430
Fiordas <simonas.juska@outlook.com>
2019-05-08 23:42:31 +0300

    Add user data update action

--- store/users.js
+++ store/users.js
@@ -6,6 +6,11 @@ export const mutations = {
   setUser(state, user) {
     state.user = user
   },
+  updateUser(state, data) {
+    Object.keys(data).forEach(key => {
+      state.user[key] = data[key]
+    })
+  },
   clearUser(state) {
     state.user = null
   }
@@ -36,6 +41,21 @@ export const actions = {
         } else {
           console.log('No user found.')
         }
+      })
+      .catch(error => console.log(error))
+  },
+  updateUserData({ commit }, { updateData, userId }) {
+    let updateMask = ''
+    Object.keys(updateData).forEach(value => {
+      updateMask += '&updateMask.fieldPaths=' + value
+    })
+    updateMask = updateMask.substring(1)
+    return this.$axios
+      .$patch(`https://firestore.googleapis.com/v1/projects/${process.env.PROJECT_ID}/databases/(default)/documents/users/${userId}?${updateMask}`, {
+        fields: this.$firestoreMap(updateData)
+      })
+      .then(() => {
+        commit('updateUser', updateData)
       })
       .catch(error => console.log(error))
   }
