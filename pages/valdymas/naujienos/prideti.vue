c5d4554b0ff072177027b5fdb459b2c06107fc82
Fiordas <simonas.juska@outlook.com>
2019-07-28 15:06:53 +0300

    Add WYSIWYG editor to news post creation

--- pages/valdymas/naujienos/prideti.vue
+++ pages/valdymas/naujienos/prideti.vue
@@ -1,27 +1,86 @@
 <template>
-  <div class='columns'>
-    <div class='column'>
-      <h1 class='subtitle'>Prideti naujieną</h1>
-      <div class='field'>
-        <label class='label'>Pavadinimas</label>
-        <div class='control'>
-          <input class='input' type='text' v-model='title'>
+  <div class='box has-background-dark'>
+    <div class='columns'>
+      <div class='column'>
+        <h1 class='title'>Prideti naujieną</h1>
+        <hr />
+        <div class='field'>
+          <label class='label'>Pavadinimas</label>
+          <div class='control'>
+            <input class='input' type='text' v-model='title' />
+          </div>
+        </div>
+        <div class='field'>
+          <label class='label'>Santrauka</label>
+          <div class='control'>
+            <textarea class='textarea' v-model='summary'></textarea>
+          </div>
+        </div>
+        <div class='field'>
+          <label class='label'>Tekstas</label>
+          <div class='control has-text-light'>
+            <no-ssr>
+              <editor-menu-bar :editor='editor' v-slot='{ commands, isActive, menu }'>
+                <div>
+                  <button class='button is-primary is-small' :class='{ "is-outlined": !isActive.bold() }' @click='commands.bold'>
+                    <b-icon icon='format-bold'></b-icon>
+                  </button>
+                  <button class='button is-primary is-small' :class='{ "is-outlined": !isActive.italic() }' @click='commands.italic'>
+                    <b-icon icon='format-italic'></b-icon>
+                  </button>
+                  <button class='button is-primary is-small' :class='{ "is-outlined": !isActive.strike() }' @click='commands.strike'>
+                    <b-icon icon='format-strikethrough'></b-icon>
+                  </button>
+                  <button class='button is-primary is-small' :class='{ "is-outlined": !isActive.heading({ level: 1 }) }' @click='commands.heading({ level: 1 })'>
+                    <b-icon icon='format-header-1'></b-icon>
+                  </button>
+                  <button class='button is-primary is-small' :class='{ "is-outlined": !isActive.heading({ level: 2 }) }' @click='commands.heading({ level: 2 })'>
+                    <b-icon icon='format-header-2'></b-icon>
+                  </button>
+                  <button class='button is-primary is-small' :class='{ "is-outlined": !isActive.heading({ level: 3 }) }' @click='commands.heading({ level: 3 })'>
+                    <b-icon icon='format-header-3'></b-icon>
+                  </button>
+                </div>
+              </editor-menu-bar>
+              <div class='content'>
+                <editor-content :editor='editor' />
+              </div>
+            </no-ssr>
+          </div>
+        </div>
+        <br />
+        <div class='buttons is-right'>
+          <nuxt-link to='/valdymas/naujienos' tag='button' class='button is-light is-inverted'>Atšaukti</nuxt-link>
+          <button @click='createNewsPost' class='button is-primary is-outlined'>Išsaugoti</button>
         </div>
       </div>
-      <div class='field'>
-        <label class='label'>Tekstas</label>
-        <div class='control'>
-          <textarea class='textarea' v-model='content'></textarea>
-        </div>
-      </div>
-      <br>
-      <button @click='createNewsPost' class='button is-primary'>Išsaugoti</button>
     </div>
   </div>
 </template>
 
 <script>
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import {
+  Blockquote,
+  HardBreak,
+  Heading,
+  OrderedList,
+  BulletList,
+  ListItem,
+  Bold,
+  Code,
+  Italic,
+  Link,
+  Strike,
+  Underline,
+  History
+} from 'tiptap-extensions'
+
 export default {
+  components: {
+    EditorMenuBar,
+    EditorContent
+  },
   computed: {
     user() {
       return this.$store.getters['users/user']
@@ -29,21 +88,64 @@ export default {
   },
   data() {
     return {
+      editor: null,
       title: '',
-      content: ''
+      summary: ''
     }
   },
   methods: {
     createNewsPost() {
       this.$store.dispatch('news/createNewsPost', {
         title: this.title,
-        content: this.content,
-        summary: this.content.substr(0, 200) + '...',
+        content: this.editor.getHTML(),
+        summary: this.summary,
         authorName: this.user.username,
         authorId: this.user.userId
       })
     }
+  },
+  mounted() {
+    this.editor = new Editor({
+      extensions: [
+        new Blockquote(),
+        new HardBreak(),
+        new Heading({ levels: [1, 2, 3] }),
+        new BulletList(),
+        new OrderedList(),
+        new ListItem(),
+        new Bold(),
+        new Code(),
+        new Italic(),
+        new Link(),
+        new Strike(),
+        new Underline(),
+        new History()
+      ],
+      content: `
+          <h1>Antraštė</h1>
+          <p>Pastraipa.</p>
+        `
+    })
+  },
+  beforeDestroy() {
+    this.editor.destroy()
   }
 }
 </script>
 
+<style>
+.ProseMirror:focus {
+  outline: none;
+}
+</style>
+
+<style scoped>
+.button {
+  margin-bottom: 10px;
+}
+.content {
+  outline: 1px solid grey;
+  padding: 10px;
+  min-height: 30vh;
+}
+</style>
